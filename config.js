412b611ae3e8a1742da656bc9c0003db5d607ae8
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-12-19 18:24:01 -0500

    Update config.js

--- config.js
+++ config.js
@@ -170,11 +170,6 @@ global.img5 = 'https://qu.ax/nWgle.jpg'
 global.imagen1 = fs.readFileSync('./media/menus/Menu3.jpg')
 global.imagen2 = fs.readFileSync('./media/menus/img1.jpg')
 global.imagen3 = fs.readFileSync('./media/menus/img2.jpg')
-  
-global.ImgRandom = [img, img2, img3, img4, img5].getRandom()
-global.imagenRandom = [imagen1, imagen2, imagen3].getRandom()
-global.gataMenu = [img, img2, img3]
-global.accounts = [canal1, canal2, canal3, canal4, canal5, tk, ig, yt, paypal, fb, ths, md, asistencia, tg].getRandom()
 
 global.canalIdGB = ["120363160031023229@newsletter", "120363169294281316@newsletter", "120363203805910750@newsletter", "120363302472386010@newsletter", "120363374372683775@newsletter", "120363336642332098@newsletter", "120363190430436554@newsletter"]
 global.canalNombreGB = ["INFINITY-WA 💫", "GB - UPDATE 🐈", "Tips sobre GataBot 🤩", "NEW PROJECT: YartexBot-MD ✨", "🌹 Pσҽƚιx ✨ Sƚҽʅʅαɾ 😎 Fυɳ", "GataBot Test", "༻🅖🅔🅝ⓑⓛ༺"]
