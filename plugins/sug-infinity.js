294d7a8a2ec92d1e98dd969021f10b2aafc5ecbd
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-12-19 18:29:02 -0500

    𝙂𝙖𝙩𝙖𝘿𝙞𝙤𝙨 - EGB

--- plugins/sug-infinity.js
+++ plugins/sug-infinity.js
@@ -26,12 +26,12 @@ const reputationTimes = [
 { reputation: 30, time: 1 * 60 * 1000 }      // 1 minuto
 ]
 
-let handler = async (m, { conn, text, usedPrefix, command }) => {
 const ADMIN_GROUP_ID = "120363317570465699@g.us"
 const CANAL_ID = WC.infinity.id
 const CANAL_LINK = WC.infinity.link
 const LEYENDA = `Este proceso es para enviar tú contenido al canal *${WC.infinity.name}*\n\n> Si deseas enviar tu contenido a otro canal usa el comando *${usedPrefix}menu*`
-     
+    
+let handler = async (m, { conn, text, usedPrefix, command }) => {
 let who = m.mentionedJid && m.mentionedJid.length > 0 ? m.mentionedJid[0] : (m.fromMe ? conn.user.jid : m.sender)
 let pp = await conn.profilePictureUrl(who, 'image').catch(_ => img5)
 let users = global.db.data.users[m.sender]
