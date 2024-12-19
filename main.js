7fde1bb10946dfa4b5ccd8718b6dd40fb1d8429e
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-12-18 21:32:04 -0500

    𝙂𝙖𝙩𝙖𝘿𝙞𝙤𝙨 - EGB

--- main.js
+++ main.js
@@ -278,8 +278,6 @@ if (global.db) setInterval(async () => {
 if (global.db.data) await global.db.write()
 if (opts['autocleartmp'] && (global.support || {}).find) (tmp = [os.tmpdir(), 'tmp', "GataJadiBot"], tmp.forEach(filename => cp.spawn('find', [filename, '-amin', '2', '-type', 'f', '-delete'])))}, 30 * 1000)}
 
-if (global.obtenerQrWeb === 1) (await import('./server.js')).default(global.conn, PORT)
-
 
 async function connectionUpdate(update) {  
 const {connection, lastDisconnect, isNewLogin} = update
